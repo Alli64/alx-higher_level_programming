082b85f8b452e0ebf80cf9d1729a0e68b41665df
Alli64 <dushimealliance1@gmail.com>
2023-09-13 17:00:11 -0700

    0x0B-python-input_output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,3 +1,8 @@
+#!/usr/bin/python3
+"""defines a function that reads a text file"""
+
+
 def read_file(filename=""):
-    with open("0-read_file.py", "r", encoding="utf-8") as f:
-        read_data = f.data()
+    """prints contents of text file UTF8 to stdout"""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 6-from_json_string.py
+"""defines a function that returns an object represented by a JSON string"""
+import json
+
+
+def def from_json_string(my_str):
+    """return the python object representation of JSON string"""
+    return json.loads(my_str)
