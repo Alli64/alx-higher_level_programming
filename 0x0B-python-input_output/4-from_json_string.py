9712d8a831d686d3b45cc92ff473bdc78f253603
Alli64 <dushimealliance1@gmail.com>
2023-09-13 17:31:08 -0700

    0x0B-python-input_output

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,9 +1,8 @@
-#!/usr/bin/python3
-#6-from_json_string.py
+#!/usr/bin/python
 """defines a function that returns an object represented by a JSON string"""
 import json
 
 
-def def from_json_string(my_str):
+def from_json_string(my_str):
     """return the python object representation of JSON string"""
     return json.loads(my_str)
