956bf7129a759a0ff43af9012e3b0361cafe28c4
Alli64 <dushimealliance1@gmail.com>
2023-12-09 17:22:12 -0800

    0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""Start link class to table in database
-"""
+"""Start link class to table in database"""
 
 import sys
 from model_state import Base, State
@@ -9,6 +8,7 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
+    """creates engine"""
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-""" prints the first State object from the database hbtn_0e_6_usa
-"""
+""" prints the first State object from the database hbtn_0e_6_usa"""
 
 import sys
 from model_state import Base, State
@@ -9,6 +8,7 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
+    """creates engine"""
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
